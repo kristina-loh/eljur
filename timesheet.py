bffdb18ce3c763bb4ae3d3d1ab506ab59b300a1e
lusya <79212237+kristina-loh@users.noreply.github.com>
2022-12-24 12:13:12 +0300

    Add files via upload

--- timesheet.py
+++ timesheet.py
@@ -6,15 +6,18 @@ with open("table.json", "r", encoding='utf-8') as my_file:
 
 def table(mp):
     cnt = 0
-    sheet = [[mp['subject']['name']]]
+    sheet = [mp['subject']['name']]
     for i in range(len(mp['lessons'])):
         if 'grades' in str(mp['lessons'][i]):
-            date = dict(mp['lessons'][i])['date']
-            theme = dict(mp['lessons'][i])['name']
-            grade = dict(mp['lessons'][i])['grades'][0]['grade']['short_name']
-            sheet.append([date, grade])
+            date = mp['lessons'][i]['date']
+            theme = mp['lessons'][i]['name']
+            grade = mp['lessons'][i]['grades'][0]['grade']['short_name']
+            if date not in sheet[1:]:
+                sheet.append({date: [grade, theme]})
+            else:
+                sheet[date] = [sheet[date][0] + '/' + grade, theme]
             cnt += 1
     return(sheet)
 
 
-# print(table(mp)[2][0])
+# print(*table(mp))
